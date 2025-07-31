324957bdaa9ddd977e74492bdb433bc7a838bb4f
raamydib@gmail.com <radib@fu-r9-p2.s19.be>
2025-07-31 15:57:20 +0200

    a

--- client/client.c
+++ client/client.c
@@ -6,41 +6,75 @@
 /*   By: radib <radib@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 14:44:40 by radib             #+#    #+#             */
-/*   Updated: 2025/07/29 14:56:24 by radib            ###   ########.fr       */
+/*   Updated: 2025/07/31 15:51:33 by radib            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minitalk.h"
 #include "../libft/libft.h"
+#include <unistd.h>
+#include <signal.h>
 
-void	sendbyte(int pid, int character)
+t_pid_and_count	g_pid_and_count;
+
+void	sendpid(int pid)
 {
 	int	i;
 
 	i = 0;
-	while (i != 8)
+	while (i < 32)
 	{
-		if ((character >> i) & 1)
-			kill(pid, SIGUSR2);
+		if ((pid >> i) & 1)
+			kill(g_pid_and_count.pid, SIGUSR2);
 		else
-			kill(pid, SIGUSR1);
-		usleep(100);
+			kill(g_pid_and_count.pid, SIGUSR1);
+		usleep(1000);
 		i++;
+	}
+}
+
+void	sendbyte(int sig)
+{
+	int	i;
+
+	i = 0;
+	if (sig == SIGUSR1)
+	{
+		while (i < 8)
+		{
+			if ((g_pid_and_count.string[g_pid_and_count.i] >> i) & 1)
+				kill(g_pid_and_count.pid, SIGUSR2);
+			else
+				kill(g_pid_and_count.pid, SIGUSR1);
+			i++;
+			usleep(1000);
+		}
+		g_pid_and_count.i++;
 	}
 }
 
 int	main(int argc, char *argv[])
 {
-	int		i;
-	int		pid;
+	struct sigaction	sa;
+	int					x;
 
-	i = 0;
+	g_pid_and_count.i = 0;
+	g_pid_and_count.string = argv[2];
+	x = getpid();
+	sa.sa_handler = sendbyte;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sigaction(SIGUSR1, &sa, NULL);
 	if (argc != 3)
 		return (0);
-	pid = ft_atoi(argv[1]);
-	while (argv[2][i])
+	g_pid_and_count.pid = ft_atoi(argv[1]);
+	sendpid(x);
+	while (1)
 	{
-		sendbyte(pid, (int)argv[2][i]);
-		i++;
+		if (g_pid_and_count.string[g_pid_and_count.i] == 0)
+		{
+			sendbyte(SIGUSR1);
+			return (0);
+		}
 	}
 }
--- minitalk.h
+++ minitalk.h
@@ -6,7 +6,7 @@
 /*   By: radib <radib@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 13:20:31 by radib             #+#    #+#             */
-/*   Updated: 2025/07/29 13:50:43 by radib            ###   ########.fr       */
+/*   Updated: 2025/07/31 15:26:35 by radib            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,11 @@
 # include <unistd.h>
 # include <signal.h>
 
-typedef struct t_bit
+typedef struct pid_and_count
 {
-	int	bit;
-	int	i;
-}	t_bit;
+	int		pid;
+	int		i;
+	char	*string;
+}	t_pid_and_count;
 
 #endif
